7a6521d60adf8a7d9fd524e4d2974487b0aaf984
Dori Steinbach <dsteinbach@email.arizona.edu>
2015-07-15 14:08:42 -0700

    couldn't use jekyll, templating with js

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,5 +1,23 @@
 var main = function() {
-  
+    $('.navigation-item').click(function() {
+        switch(this.id) {
+            case "home":
+                $('.content').innerHTML = "<p>Home</p>";
+                //$('.content').load();
+                break;
+            case "commons":
+                $('.content').load("/doriincommons.html");
+                break;
+            case "notes":
+                $('.content').load("/notesandrefs.html");
+                break;
+            case "projects":
+                $('.content').load("/projects.html");
+                break;
+            default:
+                $('.content').innerHTML = "<h1>AN ERROR HAS OCCURRED</h1>";
+        }
+    });
 }
 
 $(documents).ready(main);
